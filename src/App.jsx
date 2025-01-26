7519061fa9b0e85cbd010304e30c0b98a2eebed4
Sayed Badawy <sayedshihta0290@gmail.com>
2025-01-26 10:06:39 -0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@
 // import './App.css'
 // import TypingPractice from './components/TypingPractice'
 import TypingSprintLanding from './components/TypingSprintLanding'
+import {
+  Route, 
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from 'react-router-dom';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<TypingSprintLanding />} />,
+  )
+)
 function App() {
   
 
-  return (
-    <>
-      <TypingSprintLanding/>
-    </>
-  )
+  return  <RouterProvider router={router}/>
 }
 
 export default App
